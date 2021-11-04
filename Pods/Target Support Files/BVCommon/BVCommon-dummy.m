2af27d920a3b91347604ee0e39e6fadaec87b2c0
Blackvsion <93185961+Blackvsion@users.noreply.github.com>
2021-11-04 18:38:50 +0800

    Delete Pods directory

--- Pods/Target Support Files/BVCommon/BVCommon-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BVCommon : NSObject
-@end
-@implementation PodsDummy_BVCommon
-@end
--- Pods/Target Support Files/Pods-Example4OC/Pods-Example4OC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Example4OC : NSObject
-@end
-@implementation PodsDummy_Pods_Example4OC
-@end
